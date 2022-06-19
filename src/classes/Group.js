31b43383771f4276da5fa889311c7b79b4117658
Vladimir <admin@ttp.by>
2022-06-19 15:12:30 +0300

    feat: add Simple functional without error check

--- src/classes/Group.js
+++ src/classes/Group.js
@@ -1,12 +1,58 @@
 import User from './User.js';
-class Group {
+
+function checkId(id) {
+  return id.length === 36;
+}
+
+export default class Group {
   constructor(){
     this.room = [];
   }
 
   add(userName, age, hobbies) {
     let u = new User(userName, age, hobbies);
-    this.room.push([u.id, u]);
+    this.room.push(u);
     return u;
+  }
+
+  getAll() {
+    return this.room;
+  }
+
+  getFromId(id) {
+    if (!checkId(id)) {
+      return {
+        message: 'Id not valid',
+      }
+    }
+
+    const put = this.room.find((item) => {
+      return item.id === id;
+    });
+    console.log(put);
+    if (put) {
+      return put;
+    } else {
+      return {
+        message: 'Id not exist',
+      }
+    }
+  }
+  
+  removeById(id) {
+    const toRemove = this.room.find((item) => {
+      return item.id === id;
+    });
+    return this.room.splice(this.room.indexOf(toRemove), 1);
+  }
+
+  updateById(id, userName, age, hobbies) {
+    const toUpdate = this.room.find((item) => {
+      return item.id === id;
+    });
+    toUpdate.userName = userName;
+    toUpdate.age = age;
+    toUpdate.hobbies = hobbies;
+    return toUpdate;
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,24 @@
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import Group from './classes/Group.js';
+
+const userGroup = new Group();
+
 
 const requestHandler = (req, res) => {
-    // res.write('Hello');
-    // return res.end();
-    //console.log(res);
-    // console.log(req);
-    console.log(req.data);
-    console.log(req.meta);
-    console.log(req.requestHandler);
-    console.log(req.dirname);
-    console.log(req.rf);
-    console.log(req.body);
-    console.log(req.params);
     console.log(req.method, req.url);
-    req.on('data')
-
-    switch (req.method) {
-        case 'GET':
-            let pr = false;
-            if (req.url === '/' || req.url === '/index.html') {
-                pr = true;
+    req.on('data', (chunk) => {
+        console.log(chunk.toString());
+    })
+    const dir = path.dirname(req.url).toLowerCase();
+    const doc = path.basename(req.url);
+    //console.log('dir -->', dir, '\ndoc -->', doc);
+    switch (dir) {
+        case '/':
+        case '/test':
+            if (doc === '' || doc === 'test' || doc === 'index.html') {
+                
                 const rf = fs.createReadStream(path.join(path.dirname(fileURLToPath(import.meta.url)), '../www/index.html'), { encoding: 'utf-8' });
                 rf.on('readable', () => {
                     const data = rf.read();
@@ -31,11 +28,10 @@ const requestHandler = (req, res) => {
                     res.end();
                 })
             } else {
-               
+            
             }
-
-            if (req.url === '/index.js') {
-                pr = true;
+            if (doc === 'index.js') {
+               
                 res.contentType = 'text/javascript';
                 res.statusCode = 200;
                 const rf = fs.createReadStream(path.join(path.dirname(fileURLToPath(import.meta.url)), '../www/index.js'), { encoding: 'utf-8' });
@@ -47,9 +43,8 @@ const requestHandler = (req, res) => {
                     res.end();
                 })
             }
-
-            if (req.url === '/style.css') {
-                pr = true;
+            if (doc === 'style.css') {
+               
                 res.contentType = 'text/css';
                 res.statusCode = 200;
                 const rf = fs.createReadStream(path.join(path.dirname(fileURLToPath(import.meta.url)), '../www/style.css'), { encoding: 'utf-8' });
@@ -61,23 +56,56 @@ const requestHandler = (req, res) => {
                     res.end();
                 })
             }
-
-            if (!pr) {
+        break;
+        
+        //Work with API
+        case '/api/users':
+        case '/api':
+            switch(req.method) {
+            case 'GET':
+                console.log('doc -->', doc);
+                if (doc === 'users') {
+                    res.write(JSON.stringify(userGroup.getAll()));
+                    res.end();
+                } else {
+                    res.write(JSON.stringify(userGroup.getFromId(doc)));
+                    res.end();
+                }
+                break;
+            case 'POST':
+                let queryContent = '';
+                req.on('data', (chunk) => {
+                    if (chunk) queryContent +=chunk;
+                });
+                req.on('end', () => {
+                    console.log('end -->', queryContent);
+                    const data = JSON.parse(queryContent);
+                    console.log('data -->', data)
+                    res.write(JSON.stringify(userGroup.add(data.userName, data.age, data.hobbies)));
+                    res.end();
+                })
+                break;
+            case 'DELETE':
+                console.log(doc)
+                userGroup.removeById(doc);
+                res.statusCode = 204;
                 res.end();
+                break;
+            case 'PUT':
+                let queryContentUpdate = '';
+                req.on('data', (chunk) => {
+                    console.log(chunk);
+                    if (chunk) queryContentUpdate += chunk;
+                })
+                req.on('end', () => {
+                    const data = JSON.parse(queryContentUpdate);
+                    res.write(JSON.stringify(userGroup.updateById(doc, data.userName, data.age, data.hobbies)));
+                    res.end();
+                })
+                break;
             }
             break;
-
-        case 'PUT':
-            res.end();
-            break;
-
-        case 'POST':
-            res.end();
-            break;
-
-        case 'DELETE':
-            res.end();
-            break;
+        
     }
 
 
